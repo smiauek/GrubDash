3011237559e982277c5c60c9978506c23899644a
Piotr Smialkowski <smiaucius@gmail.com>
2022-01-21 14:02:17 -0600

    fixed delete method, removed unnecesary comments

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,12 +1,9 @@
 const path = require("path");
 
-// Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
 
-// Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
 function bodyDataHas(propertyName) {
     return function(req, res, next) {
         const {data = {}} = req.body;
@@ -106,16 +103,15 @@ function validateStatus(req, res, next) {
 };
 
 function validatePending(req, res, next) {
-    const {data: {status} = {}} = req.body;
 
-    if(status === "pending") {
-        return next();
-    };
-
-    next({
+    if(res.locals.order.status !== "pending") {
+        return next({
         status: 400,
         message: "An order cannot be deleted unless it is pending"
-    });
+     });
+    };
+
+    next();   
 };
 
 function create(req, res, next) {
